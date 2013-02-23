33510e3f1182516aaa3b9fe6bb91b5d046ff92dd
Horlicks Hung <horlicks.hung@chaogun.com>
2013-02-23 14:45:22 +0800

    add one-to-many & many-to-one relation between Product and ProductSales

--- src/test/java/com/platinumwill/test/PlatinumTest.java
+++ src/test/java/com/platinumwill/test/PlatinumTest.java
@@ -1,5 +1,7 @@
 package com.platinumwill.test;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.platinumwill.entity.Product;
+import com.platinumwill.entity.ProductSales;
 import com.platinumwill.service.PlatinumService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -26,6 +29,13 @@ public class PlatinumTest extends AbstractTest {
 	public void testPlatinumService() {
 		Product product = (Product) this.getPlatinumService().queryProduct(1);
 		getLogger().debug(product.getCreateDate().toString());
+		List<ProductSales> sales = product.getSales();
+		String isSalesNull = String.valueOf(sales == null);
+		getLogger().debug(isSalesNull);
+		if (sales != null) {
+			String salesSetSize = String.valueOf(sales.size());
+			getLogger().debug(salesSetSize);
+		}
 	}
 
 }
