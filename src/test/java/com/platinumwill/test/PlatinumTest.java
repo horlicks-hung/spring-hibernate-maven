c6c5121ac661735e1df91adc7e23c51bd522b908
Horlicks Hung <horlicks.hung@chaogun.com>
2013-03-01 15:58:20 +0800

    modify test logic

--- src/test/java/com/platinumwill/test/PlatinumTest.java
+++ src/test/java/com/platinumwill/test/PlatinumTest.java
@@ -8,12 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.platinumwill.dao.PlatinumDao;
 import com.platinumwill.entity.Product;
 import com.platinumwill.entity.ProductSales;
 import com.platinumwill.service.PlatinumService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/test-beans.xml" })
+@ContextConfiguration(locations = { "/test-beans.xml", "/beans.xml" })
 public class PlatinumTest extends AbstractTest {
 	
 	private PlatinumService platinumService;
@@ -23,6 +24,15 @@ public class PlatinumTest extends AbstractTest {
 	@Autowired
 	public void setPlatinumService(PlatinumService platinumService) {
 		this.platinumService = platinumService;
+	}
+
+	private PlatinumDao dao;
+	public PlatinumDao getDao() {
+		return this.dao;
+	}
+	@Autowired
+	public void setDao(PlatinumDao dao) {
+		this.dao = dao;
 	}
 
 	@Test
@@ -37,5 +47,23 @@ public class PlatinumTest extends AbstractTest {
 			getLogger().debug(salesSetSize);
 		}
 	}
-
+	
+	@Test
+	public void testPlatinumDao() {
+		Product product = (Product) this.getDao().queryProduct(1);
+		getLogger().debug(product.getCreateDate().toString());
+		List<ProductSales> sales = product.getSales();
+		String isSalesNull = String.valueOf(sales == null);
+		getLogger().debug(isSalesNull);
+		if (sales != null) {
+			String salesSetSize = String.valueOf(sales.size());
+			System.out.println(sales.size());
+			getLogger().debug(salesSetSize);
+		}
+		for (ProductSales sale : sales) {
+			
+			getLogger().debug(sale.getSaleBeginDate().toString());
+		}
+	}
+	
 }
